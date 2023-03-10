f09012a74cf62159cb621a3c1a9ac9e36d6a2b9e
Juan Daniel Muñoz Dueñas <88680309+DanielMunoz4190@users.noreply.github.com>
2023-03-10 09:43:38 -0600

    version casi final

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -5,10 +5,36 @@ using UnityEngine.Assertions;
 
 public class Enemy : MonoBehaviour
 {
-    public void Die()
+    [SerializeField]
+    private EnemyProyectil _disparoOriginal;
+
+    [SerializeField]
+    private float _tiempoEntreDisparos = 2;
+
+       public void Die()
     {
         // Destruye el enemigo cuando sea impactado por un proyectil
         Destroy(gameObject);
+    }
+    void shoot()
+    {
+        // Instancia un nuevo proyectil y agrega los componentes necesarios para que detecte colisiones
+        EnemyProyectil nuevoProyectil = Instantiate(
+            _disparoOriginal,
+            transform.position,
+            _disparoOriginal.transform.rotation);
+        nuevoProyectil.gameObject.AddComponent<Rigidbody>();
+        nuevoProyectil.gameObject.AddComponent<EnemyProyectil>();
+        
+    }
+    void Update()
+    {
+        // Dispara cada 2 segundos
+        if (Time.time >=  _tiempoEntreDisparos) 
+        {
+            shoot();
+           _tiempoEntreDisparos = Time.time + 2;
+        }
     }
 
 
---
+++ Assets/scripts/EnemyProyectil.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class EnemyProyectil : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5;
+
+    [SerializeField]
+    private float _tiempoDeAutodestruccion = 5;
+
+    private GUIManager _gui;
+    
+    void Start() 
+    {
+        Destroy(gameObject, _tiempoDeAutodestruccion);
+
+        GameObject guiGO = GameObject.Find("GUIManager");
+        Assert.IsNotNull(guiGO, "no hay GUIManager");
+
+        _gui = guiGO.GetComponent<GUIManager>();
+        Assert.IsNotNull(_gui, "GUIManager no tiene componente");
+    }
+
+    void Update()
+    {
+        transform.Translate(
+            0,
+            -_speed * Time.deltaTime,
+            0
+        );
+    }
+ 
+    void OnCollisionEnter(Collision c) 
+    {
+        print("ENTER " + c.transform.name);
+    }
+
+    void OnCollisionStay(Collision c) 
+    {
+        print("STAY");
+    }
+
+    void OnCollisionExit(Collision c) 
+    {
+        print("EXIT");
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        print("TRIGGER ENTER");
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        print("TRIGGER STAY");
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        print("TRIGGER EXIT");
+        _gui._texto.text = "SALI " + transform.name;
+    }
+}
